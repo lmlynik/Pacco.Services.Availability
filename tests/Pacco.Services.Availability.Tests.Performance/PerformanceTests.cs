af76b214d43754b2bbce1c40b6ca6acb3e41a7be
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-03-03 10:05:30 +0100

    perf tests 2

--- tests/Pacco.Services.Availability.Tests.Performance/PerformanceTests.cs
+++ tests/Pacco.Services.Availability.Tests.Performance/PerformanceTests.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using NBomber.CSharp;
 using NBomber.Http.CSharp;
+using Newtonsoft.Json;
+using Pacco.Services.Availability.Application.Commands;
 using Xunit;
 
 namespace Pacco.Services.Availability.Tests.Performance
@@ -36,5 +40,39 @@ namespace Pacco.Services.Availability.Tests.Performance
             NBomberRunner.RegisterScenarios(scenario)
                 .RunTest();
         }
+        
+        [Fact]
+        public void post_resources()
+        {
+            const string url = "http://localhost:5001";
+            const string stepName = "init";
+            const int duration = 3;
+            const int expectedRps = 50;
+            var endpoint = $"{url}/resources";
+
+            var step = HttpStep.Create(stepName, ctx =>
+                Task.FromResult(Http.CreateRequest("POST", endpoint)
+                    .WithBody(GetContent(new AddResource(Guid.NewGuid(), new []{"test"})))
+                    .WithCheck(response => Task.FromResult(!string.IsNullOrWhiteSpace(response.Headers.Location.AbsolutePath)))
+                    .WithCheck(response => Task.FromResult(response.IsSuccessStatusCode))));
+
+            var assertions = new[]
+            {
+                Assertion.ForStep(stepName, s => s.RPS >= expectedRps),
+                Assertion.ForStep(stepName, s => s.OkCount >= expectedRps * duration)
+            };
+
+            var scenario = ScenarioBuilder.CreateScenario("POST resources", step)
+                .WithConcurrentCopies(1)
+                .WithOutWarmUp()
+                .WithDuration(TimeSpan.FromSeconds(duration))
+                .WithAssertions(assertions);
+
+            NBomberRunner.RegisterScenarios(scenario)
+                .RunTest();
+        }
+        
+        private static StringContent GetContent(object @object)
+            => new StringContent(JsonConvert.SerializeObject(@object), Encoding.UTF8, "application/json");
     }
 }
